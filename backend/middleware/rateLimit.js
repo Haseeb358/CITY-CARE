3eb6af61b0fc7dc1dd8406f64df4235f35d0f235
Haseeb <haseebshoukat10@gmail.com>
2026-05-19 17:23:40 +0500

    Apply rateLimiter to ALL routes

--- backend/middleware/rateLimit.js
+++ backend/middleware/rateLimit.js
@@ -1,19 +1,20 @@
-// import {ratelimit} from 'express-rate-limit';
-//  got this error:  The requested module 'express-rate-limit' does not provide an export named 'ratelimit'
-import rateLimit from 'express-rate-limit';
+import rateLimit from "express-rate-limit";
 
-
-const limiter = rateLimit({
-    windowMs: 6 * 60 * 60 * 1000, // 6 hours
-    max: 4, // limit each IP to 4 requests per windowMs
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    statusCode: 429, // 429 status = Too Many Requests (RFC 6585)
+export const createRateLimiter = ({
+  windowMs = 15 * 60 * 1000, // default 15 min
+  max = 5, // default 5 requests
+  message = "Too many requests, please try again later",
+} = {}) => {
+  return rateLimit({
+    windowMs,
+    max,
+    standardHeaders: true,
+    legacyHeaders: false,
+    statusCode: 429,
     message: {
-        status: 429,
-        message: 'Too many requests from this IP, please try again after 6 hours',
-        success: false
-    }
-});
-
-export default limiter;
+      status: 429,
+      message,
+      success: false,
+    },
+  });
+};
